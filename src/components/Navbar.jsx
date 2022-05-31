9c57f37da3584489b1d342b62c62e0b6e38a03cc
AllRaDi0 <38398665+AllRaDi0@users.noreply.github.com>
2022-05-31 18:47:03 +0300

    add logo/api key

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,35 +5,40 @@ import { auth } from "./../firebaseConfig";
 import { signOut } from "firebase/auth";
 
 export default function Navbar() {
-  const [user] = useAuthState(auth);
-  return (
-    <div className="fixed-top border" style={{ backgroundColor: "whitesmoke" }}>
-      <nav className="navbar">
-        <div>
-          <img
-            src="logo192.png"
-            width={30}
-            height={30}
-            alt="logo"
-            className="ms-5"
-          />
-        </div>
-        <Link className="nav-link" to="/">
-          Home{" "}
-        </Link>
-        <div>
-          {user && (
-            <>
-              <span className="pe-4">
-                Signed is as {user.displayName || user.email}
-              </span>
-              <button className="btn btn-primary btn-sm me-3"
-              onClick={()=>{signOut(auth)}}
-              >Logout</button>
-            </>
-          )}
-        </div>
-      </nav>
-    </div>
-  );
+	const [user] = useAuthState(auth);
+	return (
+		<div className="fixed-top border" style={{ backgroundColor: "whitesmoke" }}>
+			<nav className="navbar">
+				<div>
+					<img
+						src="logo.png"
+						width={30}
+						height={30}
+						alt="logo"
+						className="ms-5"
+					/>
+				</div>
+				<Link className="nav-link" to="/">
+					Home{" "}
+				</Link>
+				<div>
+					{user && (
+						<>
+							<span className="pe-4">
+								Signed is as {user.displayName || user.email}
+							</span>
+							<button
+								className="btn btn-primary btn-sm me-3"
+								onClick={() => {
+									signOut(auth);
+								}}
+							>
+								Logout
+							</button>
+						</>
+					)}
+				</div>
+			</nav>
+		</div>
+	);
 }
